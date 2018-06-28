14d242e66527645cce88cfbfc1a56ce6a63dc425
Samuel Rafini <samuelrafini@gmail.com>
2018-06-28 08:53:47 +0200

    add get/todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+const {ObjectId} = require('mongodb')
+
 
 const app = express();
 
@@ -30,9 +32,49 @@ app.get('/todos', (req, res) => {
     })
 })
 
+// app.get('/todos/:id', (req, res) => {
+//     var id = req.params.id;
+
+//     if (!ObjectId.isValid(id)){
+//     console.log('Id is not valid');
+//         res.status(404).send();
+//     }
+
+//     Todo.findById(id).then( (todos) => {
+//         res.send({todos})
+//     }, (e) => {
+//         res.status(404).send(e)
+//     })
+
+// })
+// Andrew mead ways above myway
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectId.isValid(id)){
+    console.log('Id is not valid');
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then( (todos) => {
+
+        if(!todos) {
+            return res.status(404).send();
+        }
+
+        res.send({todos})
+
+    }).catch( (e) => {
+        res.status(400).send();
+    })
+})
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+
 
 module.exports = {app};
 
